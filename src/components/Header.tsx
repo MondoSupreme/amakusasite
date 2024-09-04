a5e5bf5cdcc74b72f226edd7119fa8234754f9c5
Don Strange <51571494+DorisTheGod@users.noreply.github.com>
2024-09-04 18:20:08 +0900

    Added in tailwindcss and begun to test it out.

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header () {
+    return (
+        <div className="container mx-auto">
+            <div className="text-3xl font-bold underline"><h1>Logo</h1></div>
+            <div className=""><h1>Navbar</h1></div>
+        </div>
+    )
+}
+
+export default Header
